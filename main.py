ddeef733ae6a058e352ad316d2025149a95524b1
Kanta Oikawa <k.oikawa@kantacky.com>
2023-06-28 23:17:16 +0900

    Add volume settings

--- main.py
+++ main.py
@@ -4,17 +4,13 @@ from notion_client import getDailyLogId, getDailyLogContentBlocks
 from slack_client import convertToSlackBlocks, postToSlackChannel
 
 def executeSlackPost(blocks):
-    try:
-        content = convertToSlackBlocks(blocks)
-        slack_config = config.getConfig('slack')
-        for properties in slack_config.values():
-            user_oauth_token = properties['user_oauth_token']
-            signing_secret = properties['signing_secret']
-            for channel_id in properties['channels'].values():
-                postToSlackChannel(user_oauth_token, signing_secret, channel_id, content)
-        print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Success")
-    except Exception as e:
-        print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Error: {e}")
+    content = convertToSlackBlocks(blocks)
+    slack_config = config.getConfig('slack')
+    for properties in slack_config.values():
+        user_oauth_token = properties['user_oauth_token']
+        signing_secret = properties['signing_secret']
+        for channel_id in properties['channels'].values():
+            postToSlackChannel(user_oauth_token, signing_secret, channel_id, content)
 
 if __name__ == '__main__':
     try:
