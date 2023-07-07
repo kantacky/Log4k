cf4aee31ee53663fc8a93ab8b5dbaad125d8a125
Kanta Oikawa <k.oikawa@kantacky.com>
2023-07-07 18:56:59 +0900

    Edit Dockerfile

--- log4k/__main__.py
+++ log4k/__main__.py
@@ -8,13 +8,16 @@ if __name__ == '__main__':
     parser.add_argument('-D', '--date', type=str, help='Date of the Log')
     args = parser.parse_args()
 
-    if args.date:
-        date = datetime(year=int(args.date[:4]), month=int(args.date[4:6]), day=int(args.date[6:]))
-
     try:
-        page_id = getDailyLogId(date)
+        if args.date:
+            date = datetime(year=int(args.date[:4]), month=int(args.date[4:6]), day=int(args.date[6:]))
+            page_id = getDailyLogId(date)
+        else:
+            page_id = getDailyLogId()
+        breakpoint()
         blocks = getDailyLogContentBlocks(page_id)
-        executeSlackPost(blocks)
+        breakpoint()
+        # executeSlackPost(blocks)
         print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Success")
     except Exception as e:
         print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Error: {e}")
