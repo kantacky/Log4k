47fa1931d5fa23011dd178e987062ebdc3b66bab
Kanta Oikawa <k.oikawa@kantacky.com>
2023-07-07 19:18:53 +0900

    Edit Dockerfile

--- log4k/__main__.py
+++ log4k/__main__.py
@@ -4,18 +4,25 @@ from log4k.notion_client import getDailyLogId, getDailyLogContentBlocks
 from log4k.executor import executeSlackPost
 
 if __name__ == '__main__':
-    parser = ArgumentParser()
-    parser.add_argument('-D', '--date', type=str, help='Date of the Log')
-    args = parser.parse_args()
-
     try:
+        parser = ArgumentParser()
+        parser.add_argument('-D', '--date', type=str, help='Date of the Log')
+        args = parser.parse_args()
+
         if args.date:
             date = datetime(year=int(args.date[:4]), month=int(args.date[4:6]), day=int(args.date[6:]))
             page_id = getDailyLogId(date)
         else:
             page_id = getDailyLogId()
+
         blocks = getDailyLogContentBlocks(page_id)
-        executeSlackPost(blocks, date)
+
+        if date:
+            executeSlackPost(blocks, date)
+        else:
+            executeSlackPost(blocks)
+
         print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Success")
+
     except Exception as e:
         print(f"{datetime.now().strftime('%Y-%m-%dT%H:%M:%S+0900')} Error: {e}")
